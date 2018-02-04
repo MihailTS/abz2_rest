1f794b2cc731aa4ebb3912d60f948d023356f830
MihailTS <tseluiko.m@gmail.com>
2018-02-04 19:50:36 +0300

    Change head method test

--- tests/Unit/EmployeeTest.php
+++ tests/Unit/EmployeeTest.php
@@ -34,4 +34,27 @@ class EmployeeTest extends TestCase
         $firstLevelEmployee = factory(Employee::class)->create(['head_id' => ($rootEmployee->id)]);
         $this->assertFalse($firstLevelEmployee->isHeadOf($rootEmployee));
     }
+
+    public function test_changeHead_Normal()
+    {
+        $employeeSubordinate = factory(Employee::class)->create();
+        $employeeNewHead = factory(Employee::class)->create();
+
+        $employeeSubordinate->changeHead($employeeNewHead);
+
+        $this->assertTrue($employeeSubordinate->head->id == $employeeNewHead->id);
+    }
+
+    /**
+     * если новый начальник является подчиненным текущего сотрудника
+     */
+    public function test_changeHead_IfSubordinate()
+    {
+        $this->expectExceptionMessage('parent');
+
+        $employeeSubordinate = factory(Employee::class)->create();
+        $employeeNewHead = factory(Employee::class)->create(['head_id' => $employeeSubordinate->id]);
+
+        $employeeSubordinate->changeHead($employeeNewHead);
+    }
 }

8146c0a06230147b7975dd60a4b4e7d85fde16c3
Tseluiko <tseluiko.m@gmail.com>
2018-02-09 17:16:05 +0300

    require fractal

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Spatie\Fractal\FractalServiceProvider::class,
         /*
          * Application Service Providers...
          */
